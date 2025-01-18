57456071a7cfc5f25f267a824e288da70cf32c3a
Ayush Gupta <ayush27gupta@gmail.com>
2025-01-19 00:21:17 +0530

    Added Table

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,43 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Searchbar from "./components/searchbar/Searchbar";
 import Sidebar from "./components/sidebar/Sidebar";
+import Table from "./components/table/Table";
 
 const App = () => {
   const [count, setCount] = useState(0);
+
+  const data = [
+    {
+      inputColumn: "Oct 12, 2024 at 14:08 PM",
+      actionColumn: {
+        type: "text",
+        value: "BitScale Evaluation - Account relevant",
+        icon: "/load.svg",
+      },
+      enrichCompany: "BitScale Evaluation - Account relevant",
+      enrichIcon: "fas fa-check-circle",
+    },
+    {
+      inputColumn: "Oct 12, 2024 at 14:08 PM",
+      actionColumn: {
+        type: "error",
+        value: "cell data size exceeds limit",
+        icon: "/warn.svg",
+      },
+      enrichCompany: "BMW Evaluation - Relevancy check",
+      enrichIcon: "fas fa-building",
+    },
+    {
+      inputColumn: "Oct 12, 2024 at 14:08 PM",
+      actionColumn: {
+        type: "link",
+        value: "https://www.linkedin.com/bitScale",
+        icon: "/link.svg",
+      },
+      enrichCompany: "Google Evaluation - Lilevancy check",
+      enrichIcon: "fas fa-industry",
+    },
+  ];
 
   return (
     <>
@@ -15,6 +49,7 @@ const App = () => {
           <Sidebar />
           <div className="content">
             <Searchbar />
+            <Table data={data} />
           </div>
         </div>
       </div>
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,8 +1,80 @@
 import React from "react";
 import "./table.css";
 
-const Table = () => {
-  return <div></div>;
+const Table = (props) => {
+  const { data } = props;
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+          <th>
+            <div className="d-flex align-items-center justify-content-start">
+              <div className="table-head-img">
+                <img src="/text.svg" alt="" />
+              </div>
+              <div>Input Column</div>
+            </div>
+          </th>
+          <th>
+            <div className="d-flex align-items-center justify-content-start">
+              <div className="table-head-img">
+                <img src="/chat.svg" alt="" />
+              </div>
+              <div>Action Column</div>{" "}
+            </div>
+          </th>
+          <th>
+            <div className="d-flex align-items-center justify-content-start">
+              <div className="table-head-img">
+                <img src="/apolo.svg" alt="" />
+              </div>
+              <div>Enrich Company</div>
+            </div>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index}>
+            <th className="td-width" scope="row">
+              {index + 1}
+            </th>
+            <td className="td-width">
+              <img src="/play.svg" alt="" />
+            </td>
+            <td>
+              <i className={item.inputIcon} style={{ marginRight: "5px" }}></i>
+              {item.inputColumn}
+            </td>
+            <td>
+              <img
+                src={`${item.actionColumn.icon}`}
+                alt=""
+                style={{ marginRight: "0.3rem" }}
+              />
+              {item.actionColumn.type === "link" ? (
+                <a
+                  href={item.actionColumn.value}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {item.actionColumn.value}
+                </a>
+              ) : (
+                <span>{item.actionColumn.value}</span>
+              )}
+            </td>
+            <td>
+              <i className={item.enrichIcon} style={{ marginRight: "5px" }}></i>
+              {item.enrichCompany}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default Table;
